867edce83f4369e2a85260f340b3f3d1436bb91b
235 <krahs@gmai.com>
2024-12-13 21:23:50 +0500

    add animate bg in pages

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,26 +1,45 @@
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+
+import { Outlet, useLocation } from 'react-router-dom'
 
 import Sidebar from '../Sidebar/'
 import './index.scss'
 
 const Layout = () => {
-  return (
-    <div className="App">
-      <Sidebar />
-      <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+  const location = useLocation()
 
-        <Outlet />
-        <span className="tags bottom-tags">
+  const getPageClass = () => {
+    const path = location.pathname.substring(1).split('/')[0]
+    if (!path) return 'home'
+    return path
+  }
+
+  useEffect(() => {
+    const pageClass = getPageClass()
+    document.body.className = `${pageClass}-page`
+
+    return () => {
+      document.body.className = ''
+    }
+  }, [location])
+
+  return (
+      <div className="App">
+        <Sidebar />
+        <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
+
+          <Outlet />
+          <span className="tags bottom-tags">
           <div id="stars"></div>
           <div id="stars2"></div>
           <div id="stars3"></div>
-          &lt;/body&gt;
-          <br />
+            &lt;/body&gt;
+            <br />
           <span className="bottom-tag-html">&lt;/html&gt;</span>
         </span>
+        </div>
       </div>
-    </div>
   )
 }
 
